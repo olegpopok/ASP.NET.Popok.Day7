a89fdf63d63eb76e80f9029ea200473c158c9469
olegpopok <olegpopo1994@gmail.com>
2015-07-30 18:26:45 +0300

    2.0

--- Task3.Library/CustomQueue.cs
+++ Task3.Library/CustomQueue.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Task3.Library
 {
-    public class CustomQueue<T>
+    public class CustomQueue<T> : IEnumerable<T>
     {
         private const int _DefaultCapasity = 4;
 
@@ -14,6 +15,7 @@ namespace Task3.Library
         private int _size;
         private int _head;
         private int _tail;
+        private int _version;
 
         public CustomQueue()
         {
@@ -36,6 +38,7 @@ namespace Task3.Library
             }
             _array = new T[_DefaultCapasity];
             _size = 0;
+            _version = 0;
 
             foreach (T item in collection)
             {
@@ -62,6 +65,7 @@ namespace Task3.Library
             _array[_tail] = item;
             _tail = (_tail + 1) % _array.Length;
             _size++;
+            _version++;
         }
 
         public T DeQueue()
@@ -74,6 +78,7 @@ namespace Task3.Library
             _array[_head] = default(T);
             _head = (_head + 1) % _array.Length;
             _size--;
+            _version++;
             return removed;
         }
 
@@ -84,6 +89,16 @@ namespace Task3.Library
                 throw new InvalidOperationException("Queue is empty.");
             }
             return _array[_head];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
 
         private void SetCapasity(int capasity)
@@ -108,6 +123,84 @@ namespace Task3.Library
             _head = 0;
             _tail = _size;
         }
-        
+
+        private T GetElement(int index)
+        {
+            return _array[(_head + index) % _array.Length];
+        }
+
+        public class Enumerator : IEnumerator<T>, System.Collections.IEnumerator
+        {
+            private CustomQueue<T> _queue;
+            private int _index;
+            private T _currentElement;
+            private int _version;
+
+            internal Enumerator(CustomQueue<T> queue)
+            {
+                _queue = queue;
+                _version = _queue._version;
+                _index = -1;
+                _currentElement = default(T);
+
+            }
+
+            void IDisposable.Dispose()
+            {
+                _index = -2;
+                _currentElement = default(T);
+            }
+
+            public bool MoveNext()
+            {
+                if (_version != _queue._version)
+                {
+                    throw new InvalidOperationException("Enumerator version feiled.");
+                }
+                if (_index == -2)
+                {
+                    return false;
+                }
+                _index++;
+
+                if (_index == _queue._size)
+                {
+                    return false;
+                }
+
+                _currentElement = _queue.GetElement(_index);
+                return true;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    if (_index < 0)
+                    {
+                        if(_index == -1)
+                        {
+                            throw new InvalidOperationException("Enum not started.");
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException("Enum ended.");
+                        }
+                    }
+
+                    return _currentElement;
+                }
+            }
+
+            public void Reset()
+            {
+                if(_version != _queue._version)
+                {
+                    throw new InvalidOperationException("Enumerator version feiled.");
+                }
+                _index = -1;
+                _currentElement = default(T);
+            }
+        }
     }
 }
